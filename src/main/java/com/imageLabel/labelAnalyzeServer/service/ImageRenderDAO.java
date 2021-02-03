5d27d641b5862541c4c60dbfc345b740aed8ab97
Hakeyon <20181018@sungshin.ac.kr>
2021-02-04 00:04:57 +0900

    Rendering Url 넘겨주기

--- src/main/java/com/imageLabel/labelAnalyzeServer/service/ImageRenderDAO.java
+++ src/main/java/com/imageLabel/labelAnalyzeServer/service/ImageRenderDAO.java
@@ -3,11 +3,22 @@ package com.imageLabel.labelAnalyzeServer.service;
 import java.util.ArrayList;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class ImageRenderDAO {
 
 	public ArrayList<String> getImageList(JSONArray jsonArray){
 		ArrayList<String> imageUrl = new ArrayList<>();
+
+		for(int i = 0 ; i<jsonArray.length(); i++){
+			JSONObject imageId = (JSONObject)jsonArray.get(i);
+			String imgIdUrl = "http://localhost:8081/image/"+imageId.getString("imageId");
+			imageUrl.add(imgIdUrl);
+		}
+
+		for(int i= 0; i< imageUrl.size(); i++){
+			System.out.println(imageUrl.get(i));
+		}
 
 		return imageUrl;
 	}
